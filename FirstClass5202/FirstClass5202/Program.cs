48ecf0cb4ddc1a7b1d5bcbce06b7cf19b6d690f5
Danielle Kumeroa <9999846@student.toiohomai.ac.nz>
2019-07-24 11:27:24 +1200

    hello world

---
+++ FirstClass5202/FirstClass5202/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass5202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Please enter your last name: ");
+            string lname = Console.ReadLine();
+            Console.WriteLine("Hello {0} {1}, nice to meet you.", name, lname);
+            Console.ReadLine();
+            Console.WriteLine("hello " + name + " " + lname + ", nice to meet you.");
+            Console.WriteLine($"hello {name} {lname}, nice to meet you.");
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
